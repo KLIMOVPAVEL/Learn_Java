cfbfeaeb9f88d8441d3375e9b9b906c355af3e1e
KLIMOVPAVEL <gornii2013@yandex.ru>
2017-02-25 16:10:37 +0300

    Adding operation '+', '-', '/', '*'.

--- Calculate.java
+++ Calculate.java
@@ -2,5 +2,12 @@ public class Calculate{
 	public static void main(String[] arg)
 	{
 	System.out.println("Calculate");
+	float Ffirst = Float.valueOf(arg[0]);
+	float Fsecond = Float.valueOf(arg[1]);
+	float Fsum = Ffirst + Fsecond;
+	System.out.println("sum " + Fsum);
+	
+	
+
 	}
 }
